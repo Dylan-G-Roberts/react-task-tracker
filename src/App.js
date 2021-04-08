c222cc31ce2a0b47aad5862dcb09c2de26728900
Dylan Roberts <dylangardnerroberts@gmail.com>
2021-04-08 20:50:09 -0300

    master - Installed react-icons for fa

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
+import { useState } from 'react' 
 import Header from './components/Header'
-
+import Tasks from './components/Tasks'
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+        id: 1,
+        text: 'Docs',
+        day: 'February 5th at 2:30pm',
+        reminder:true
+    },
+    {
+        id: 2,
+        text: 'Docs',
+        day: 'February 5th at 2:30pm',
+        reminder:true
+    },
+    {
+        id: 3,
+        text: 'Docs',
+        day: 'February 5th at 2:30pm',
+        reminder:true
+    }
+])
+
   return (
     <div className="App">
       <Header/>
+      <Tasks tasks={tasks}/>
     </div>
   );
 }
